8894dbd18c8f0768740795f4e64d3aa42e2d92a9
jjlavigne <155919746+jjlavigne@users.noreply.github.com>
2025-05-27 09:32:59 -0700

    Update server.js

--- server.js
+++ server.js
@@ -43,9 +43,7 @@ const dataFilePath = path.join(__dirname, 'data.json');
 // Utility to deep merge objects
 function deepMerge(target, updates) {
   for (const key in updates) 
-    if ( updates[key] instanceof Object &&
-         key in target &&
-         target[key] instanceof Object ) 
+    if ( updates[key] instanceof Object ) 
 
       deepMerge(target[key], updates[key]);
     else 
